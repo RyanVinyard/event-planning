6456373593b026a9c4778ad57fe4a959f17bbc7a
Ryan Vinyard <rav.ryanvinyard@gmail.com>
2016-09-09 18:51:56 -0700

    add notation to broken front end

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,26 @@
+import java.io.Console;
+import java.util.List;
+
 public class App {
   public static void main(String[] args) {
-
-    System.out.println("Good job");
+    Console myConsole = System.console();
+    System.out.println("Hello there! Glad to see you're planing an event!");
+    System.out.println("I'll need to get some info from you to estimate a cost, however.");
+    System.out.println("How many people do you expect to attend?");
+    String stringPeople = myConsole.readLine();
+    int people = Integer.parseInt(stringPeople);
+    System.out.println("What sort of drinks are you planning to provide? None, beer, a paid bar, or an open bar?");
+    String drink = myConsole.readLine();
+    System.out.println("What sort of food are you going to provide? None, snacks, appetizers, dinner, or a buffet?");
+    String food = myConsole.readLine();
+    System.out.println("What kind of entertainment are you going to have? Your self, a dj, local music, or a big name band?");
+    String entertainment = myConsole.readLine();
+    System.out.println("Finally, how long (in hours) do you expect this to go for?");
+    String stringTime = myConsole.readLine();
+    int time = Integer.parseInt(stringPeople);
+    EventPlanning eventPlanning = new EventPlanning();
+    List<Object> eventPlanningResult = eventPlanning.runEventPlanning(people, drink, food, entertainment, time);
+    System.out.println(eventPlanningResult);
 
 
       }
