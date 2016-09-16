f1b7ed19c9010c6ca35b792fbe7a13f269c788a5
Ryan Vinyard <rav.ryanvinyard@gmail.com>
2016-09-15 17:26:44 -0700

    begin code rewrite

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,26 +4,28 @@ import java.util.ArrayList;
 
 public class App {
   public static void main(String[] args) {
-    Console myConsole = System.console();
-    System.out.println("Hello there! Glad to see you're planing an event!");
-    System.out.println("I'll need to get some info from you to estimate a cost, however.");
-    System.out.println("How many people do you expect to attend?");
-    String stringPeople = myConsole.readLine();
-    int people = Integer.parseInt(stringPeople);
-    System.out.println("What sort of drinks are you planning to provide? None, beer, a paid bar, or an open bar?");
-    String drink = myConsole.readLine();
-    System.out.println("What sort of food are you going to provide? None, snacks, appetizers, dinner, or a buffet?");
-    String food = myConsole.readLine();
-    System.out.println("What kind of entertainment are you going to have? Your self, a dj, local music, or a big name band?");
-    String entertainment = myConsole.readLine();
-    System.out.println("Finally, how long (in hours) do you expect this to go for?");
-    String stringTime = myConsole.readLine();
-    int time = Integer.parseInt(stringTime);
-    EventPlanning eventPlanning = new EventPlanning();
-    List<Object> eventPlanningResult = eventPlanning.runEventPlanning(people, drink, food, entertainment, time);
-    for (Object eventItem : eventPlanningResult) {
-      System.out.println(eventItem);
-    }
+
+
+    // Console myConsole = System.console();
+    // System.out.println("Hello there! Glad to see you're planing an event!");
+    // System.out.println("I'll need to get some info from you to estimate a cost, however.");
+    // System.out.println("How many people do you expect to attend?");
+    // String stringPeople = myConsole.readLine();
+    // int people = Integer.parseInt(stringPeople);
+    // System.out.println("What sort of drinks are you planning to provide? None, beer, a paid bar, or an open bar?");
+    // String drink = myConsole.readLine();
+    // System.out.println("What sort of food are you going to provide? None, snacks, appetizers, dinner, or a buffet?");
+    // String food = myConsole.readLine();
+    // System.out.println("What kind of entertainment are you going to have? Your self, a dj, local music, or a big name band?");
+    // String entertainment = myConsole.readLine();
+    // System.out.println("Finally, how long (in hours) do you expect this to go for?");
+    // String stringTime = myConsole.readLine();
+    // int time = Integer.parseInt(stringTime);
+    // EventPlanning eventPlanning = new EventPlanning();
+    // List<Object> eventPlanningResult = eventPlanning.runEventPlanning(people, drink, food, entertainment, time);
+    // for (Object eventItem : eventPlanningResult) {
+    //   System.out.println(eventItem);
+    // }
     //This front end logic does not work. It is returning an array containing only the first entry (number of people) run through the eventPlanning method.
 
       }
--- src/main/java/EventPlanning.java
+++ src/main/java/EventPlanning.java
@@ -2,57 +2,86 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-
-
-
 public class EventPlanning {
+  private Integer mPeople;
+  private String mFood;
+  private String mDrinks;
+  private String mEntertainment;
 
-  public List<Object> runEventPlanning(Integer howManyPeople, String howMuchDrink, String howMuchFood, String howMuchEntertainment, int howLong){
-    List<Object> result = new ArrayList<Object>();
-    if (howManyPeople.equals(1)) {
-      result.add(0);
-    } else if (howManyPeople > 1 && howManyPeople < 30) {
-      result.add(0);
-    } else if (howManyPeople >= 30 && howManyPeople < 80) {
-      result.add((80*howLong));
-    } else if (howManyPeople >= 80 && howManyPeople < 300) {
-      result.add(1000);
-    } else if (howManyPeople >= 300) {
-      result.add(10000);
-    }
-
-    if (howMuchDrink.equals("none")) {
-      result.add(0);
-    } else if (howMuchDrink.equals("beer")) {
-      result.add((10*howManyPeople));
-    } else if (howMuchDrink.equals("paid bar")) {
-      result.add((100*howLong));
-    } else if (howMuchDrink.equals("open bar")) {
-      result.add(((100)+(25*howManyPeople)));
-    }
-
-    if (howMuchFood.equals("none")) {
-      result.add(0);
-    } else if (howMuchFood.equals("snacks")) {
-      result.add((30+(2*howManyPeople)));
-    } else if (howMuchFood.equals("appetizers")) {
-      result.add((50+(5*howManyPeople)));
-    } else if (howMuchFood.equals("dinner")) {
-      result.add((100+(10*howManyPeople)));
-    } else if (howMuchFood.equals("buffet")) {
-      result.add((250+(15*howManyPeople)));
-    }
-
-    if (howMuchEntertainment.equals("self")) {
-      result.add(0);
-    } else if (howMuchEntertainment.equals("dj")) {
-      result.add((80*howLong));
-    } else if (howMuchEntertainment.equals("local music")) {
-      result.add((100*howLong));
-    } else if (howMuchEntertainment.equals("big name band")) {
-      result.add((5000+(10*howLong)));
-    }
-    return result;
-
+  public EventPlanning(Integer people, String food, String drinks, String entertainment) {
+    mPeople = people;
+    mFood = food;
+    mDrinks = drinks;
+    mEntertainment = entertainment;
   }
+
+  public Integer getPeople() {
+    return mPeople;
+  }
+
+  public String getFood() {
+    return mFood;
+  }
+
+  public String getDrinks() {
+    return mDrinks;
+  }
+
+  public String getEntertainment() {
+    return mEntertainment;
+  }
+
+
 }
+
+// public class EventPlanning {
+//
+//   public List<Object> runEventPlanning(Integer howManyPeople, String howMuchDrink, String howMuchFood, String howMuchEntertainment, int howLong){
+//     List<Object> result = new ArrayList<Object>();
+//     if (howManyPeople.equals(1)) {
+//       result.add(0);
+//     } else if (howManyPeople > 1 && howManyPeople < 30) {
+//       result.add(0);
+//     } else if (howManyPeople >= 30 && howManyPeople < 80) {
+//       result.add((80*howLong));
+//     } else if (howManyPeople >= 80 && howManyPeople < 300) {
+//       result.add(1000);
+//     } else if (howManyPeople >= 300) {
+//       result.add(10000);
+//     }
+//
+//     if (howMuchDrink.equals("none")) {
+//       result.add(0);
+//     } else if (howMuchDrink.equals("beer")) {
+//       result.add((10*howManyPeople));
+//     } else if (howMuchDrink.equals("paid bar")) {
+//       result.add((100*howLong));
+//     } else if (howMuchDrink.equals("open bar")) {
+//       result.add(((100)+(25*howManyPeople)));
+//     }
+//
+//     if (howMuchFood.equals("none")) {
+//       result.add(0);
+//     } else if (howMuchFood.equals("snacks")) {
+//       result.add((30+(2*howManyPeople)));
+//     } else if (howMuchFood.equals("appetizers")) {
+//       result.add((50+(5*howManyPeople)));
+//     } else if (howMuchFood.equals("dinner")) {
+//       result.add((100+(10*howManyPeople)));
+//     } else if (howMuchFood.equals("buffet")) {
+//       result.add((250+(15*howManyPeople)));
+//     }
+//
+//     if (howMuchEntertainment.equals("self")) {
+//       result.add(0);
+//     } else if (howMuchEntertainment.equals("dj")) {
+//       result.add((80*howLong));
+//     } else if (howMuchEntertainment.equals("local music")) {
+//       result.add((100*howLong));
+//     } else if (howMuchEntertainment.equals("big name band")) {
+//       result.add((5000+(10*howLong)));
+//     }
+//     return result;
+//
+//   }
+// }
